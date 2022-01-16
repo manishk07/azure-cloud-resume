1d39ce802fe07e9ccab45ca1b8560b876d0f3954
Manish Singh <manishk07@live.in>
2022-01-17 03:37:45 +0530

    configured visitor counter

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,11 @@ window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 })
 
-const functionAPI = '';
+const functionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionAPI).then(response => {
+    fetch(functionApi).then(response => {
         return response.json()
     }).then(response => {
         console.log("Website called function API.");
