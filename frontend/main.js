29473b4efccf1bafaff77ce30fef6f0c021c13ad
Manish Singh <86804268+manishk07@users.noreply.github.com>
2022-02-18 02:16:48 +0530

    Created Workflows

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,12 @@ window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 })
 
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApiUrl = 'https://getresumecounter7.azurewebsites.net/api/GetResumeCounter?code=syBMk6owaSc8SsGsjUqCS6pqFdnrdqVdRLLEL4UsQE4DMOKQKKxXWg==';
+const localFunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response => {
         console.log("Website called function API.");
